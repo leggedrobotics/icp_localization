c6a190f624595d74ca7ba59e4719489f304ce75c
jelavice <jelavice@ethz.ch>
2021-08-17 09:48:38 +0200

    Renames, clean up the set init pose service

--- src/ICPlocalization.cpp
+++ src/ICPlocalization.cpp
@@ -29,7 +29,9 @@
 
 namespace icp_loco {
 
+namespace {
 const double kRadToDeg = 180.0 / M_PI;
+}
 
 ICPlocalization::ICPlocalization(const ros::NodeHandle &nh) :
 		nh_(nh), rangeDataAccumulator_(nh) {
@@ -44,7 +46,7 @@ ICPlocalization::~ICPlocalization() {
 	icpWorker_.join();
 	Rigid3d lastPose(lastPosition_, lastOrientation_);
 	std::cout << "ICP_LOCO: Last transform map to range sensor: \n";
-	std::cout<< lastPose.asString() << "\n";
+	std::cout << lastPose.asString() << "\n";
 }
 
 void ICPlocalization::setMapCloud(const Pointcloud &map) {
@@ -55,63 +57,50 @@ void ICPlocalization::setMapCloud(const Pointcloud &map) {
 	std::cout << "Map size is: " << mapCloud_.points.size() << std::endl;
 }
 
-void ICPlocalization::setInitialPose(const Eigen::Vector3d &p,
-		const Eigen::Quaterniond &q) {
-	std::cout << "INit pose set to, xyz: " << p.transpose();
-	std::cout << ", q: " << q.coeffs().transpose() << ", rpy: "
-			<< kRadToDeg * icp_loco::toRPY(q).transpose() << " deg \n";
+void ICPlocalization::setInitialPose(const Eigen::Vector3d &p, const Eigen::Quaterniond &q) {
+	std::cout << "Init pose set to, xyz: " << p.transpose();
+	std::cout << ", q: " << q.coeffs().transpose() << ", rpy: " << kRadToDeg * icp_loco::toRPY(q).transpose()
+			<< " deg \n";
 	lastPosition_ = p;
 	lastOrientation_ = q;
 	imuTracker_->setInitialPose(p, q);
 	tfPublisher_->setInitialPose(p, q);
 }
 
-void ICPlocalization::set2DPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
-	try
-	{
-		// Get transform between range_sensor and base_link
-		geometry_msgs::TransformStamped base_link_to_range_sensor = tf_buffer_->lookupTransform("base_link", 
-														"range_sensor", ros::Time(0), ros::Duration(1.0));
+void ICPlocalization::set2DPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg) {
+	try {
 		geometry_msgs::Pose pose_received = msg->pose.pose;
-
-		// Transform pose
-		tf2::doTransform(pose_received, pose_received, base_link_to_range_sensor);
-
-		tf::pointMsgToEigen(msg->pose.pose.position, set_pose_vector_);
-		tf::quaternionMsgToEigen(pose_received.orientation, set_pose_quaternion_);
-		setPoseFlag = true;	
-	}
-	catch(const std::exception& e)
-	{
+		tf::pointMsgToEigen(msg->pose.pose.position, userSetPosition_);
+		tf::quaternionMsgToEigen(pose_received.orientation, userSetQuaternion_);
+		isSetPoseFromUser_ = true;
+	} catch (const std::exception &e) {
 		std::cerr << "Caught exception while setting 2D pose: " << e.what() << '\n';
 	}
+	std::cout << "User set pose to :" << Rigid3d(userSetPosition_,userSetQuaternion_).asString() << std::endl;
+
 }
 
 void ICPlocalization::initializeInternal() {
 
 	imuTracker_ = std::make_shared<ImuTracker>();
 	frameTracker_ = std::make_shared<FrameTracker>(imuTracker_);
-	tfPublisher_ = std::make_shared<TfPublisher>(nh_, frameTracker_,
-			imuTracker_);
+	tfPublisher_ = std::make_shared<TfPublisher>(nh_, frameTracker_, imuTracker_);
 
 	lastPosition_.setZero();
 	lastOrientation_.setIdentity();
 
-	registeredCloudPublisher_ = nh_.advertise<sensor_msgs::PointCloud2>(
-			"registered_cloud", 1, true);
-	posePub_ = nh_.advertise<geometry_msgs::PoseStamped>("range_sensor_pose",
-			1, true);
+	registeredCloudPublisher_ = nh_.advertise<sensor_msgs::PointCloud2>("registered_cloud", 1, true);
+	posePub_ = nh_.advertise<geometry_msgs::PoseStamped>("range_sensor_pose", 1, true);
 	icp_.setDefault();
 
 	// Initialite tf listener
-	tf_buffer_.reset(new tf2_ros::Buffer(ros::Duration(10)));
-  	tf_listener_.reset(new tf2_ros::TransformListener(*tf_buffer_));
+	tfBuffer_.reset(new tf2_ros::Buffer(ros::Duration(10)));
+	tfListener_.reset(new tf2_ros::TransformListener(*tfBuffer_));
 }
 
 void ICPlocalization::initialize() {
 
-	const std::string configFileIcp = nh_.param<std::string>("icp_config_path",
-			"");
+	const std::string configFileIcp = nh_.param<std::string>("icp_config_path", "");
 	try {
 		std::ifstream in(configFileIcp);
 		if (!in.is_open()) {
@@ -123,8 +112,7 @@ void ICPlocalization::initialize() {
 		std::cout << e.what() << std::endl;
 	}
 
-	const std::string configFileFilters = nh_.param<std::string>(
-			"input_filters_config_path", "");
+	const std::string configFileFilters = nh_.param<std::string>("input_filters_config_path", "");
 	try {
 		std::ifstream in(configFileFilters);
 		if (!in.is_open()) {
@@ -137,18 +125,15 @@ void ICPlocalization::initialize() {
 	}
 
 	std::string rangeDataTopic = "";
-	if (!nh_.param<std::string>("icp_localization/range_data_topic",
-			rangeDataTopic, "")) {
+	if (!nh_.param<std::string>("icp_localization/range_data_topic", rangeDataTopic, "")) {
 		ROS_ERROR_STREAM("failed to load range data topic");
 	}
 	std::string imuDataTopic = "";
-	if (!nh_.param<std::string>("icp_localization/imu_data_topic", imuDataTopic,
-			"")) {
+	if (!nh_.param<std::string>("icp_localization/imu_data_topic", imuDataTopic, "")) {
 		ROS_ERROR_STREAM("failed to load imu data topic");
 	}
 	std::string odometryDataTopic = "";
-	if (!nh_.param<std::string>("icp_localization/odometry_data_topic",
-			odometryDataTopic, "")) {
+	if (!nh_.param<std::string>("icp_localization/odometry_data_topic", odometryDataTopic, "")) {
 		ROS_ERROR_STREAM("failed to load odometry data topic");
 	}
 
@@ -156,8 +141,7 @@ void ICPlocalization::initialize() {
 	std::cout << "imu data topic: " << imuDataTopic << std::endl;
 
 	isUseOdometry_ = nh_.param<bool>("icp_localization/is_use_odometry", false);
-	std::cout << "Is use odometry: " << std::boolalpha << isUseOdometry_
-			<< "\n";
+	std::cout << "Is use odometry: " << std::boolalpha << isUseOdometry_ << "\n";
 
 	tfPublisher_->setOdometryTopic(odometryDataTopic);
 	tfPublisher_->setImuTopic(imuDataTopic);
@@ -169,17 +153,13 @@ void ICPlocalization::initialize() {
 	imuTracker_->setGravityVectorFilterTimeConstant(gravityVectorFilterTimeConstant);
 
 	const std::string imuLidarPrefix = "calibration/imu_to_range_sensor/";
-	const Rigid3d imuToRangeSensor = Rigid3d(
-			getPositionFromParameterServer(nh_, imuLidarPrefix),
+	const Rigid3d imuToRangeSensor = Rigid3d(getPositionFromParameterServer(nh_, imuLidarPrefix),
 			getOrientationFromParameterServer(nh_, imuLidarPrefix));
 
-	const std::string odometrySourceLidarPrefix =
-			"calibration/odometry_source_to_range_sensor/";
-	const Rigid3d odometrySourceToRangeSensor = Rigid3d(
-			getPositionFromParameterServer(nh_, odometrySourceLidarPrefix),
+	const std::string odometrySourceLidarPrefix = "calibration/odometry_source_to_range_sensor/";
+	const Rigid3d odometrySourceToRangeSensor = Rigid3d(getPositionFromParameterServer(nh_, odometrySourceLidarPrefix),
 			getOrientationFromParameterServer(nh_, odometrySourceLidarPrefix));
-	frameTracker_->setTransformOdometrySourceToRangeSensor(
-			odometrySourceToRangeSensor);
+	frameTracker_->setTransformOdometrySourceToRangeSensor(odometrySourceToRangeSensor);
 	frameTracker_->setTransformImuToRangeSensor(imuToRangeSensor);
 	frameTracker_->setIsUseOdometryForRangeSensorPosePrediction(isUseOdometry_);
 
@@ -189,21 +169,16 @@ void ICPlocalization::initialize() {
 	std::cout << "Min num odom measurements before ready: " << minNumOdomMeasurements << std::endl;
 
 	std::cout << "Calibration: \n";
-	std::cout << "imu to range sensor: " << imuToRangeSensor.asString()
-			<< "\n\n";
-	std::cout << "odometry source to range sensor: "
-			<< odometrySourceToRangeSensor.asString() << "\n\n";
+	std::cout << "imu to range sensor: " << imuToRangeSensor.asString() << "\n\n";
+	std::cout << "odometry source to range sensor: " << odometrySourceToRangeSensor.asString() << "\n\n";
 
 	RangeDataAccumulatorParamRos rangeDataAccParam;
-	rangeDataAccParam.numAccumulatedRangeData_ = nh_.param<int>(
-			"icp_localization/num_accumulated_range_data", 1);
-	rangeDataAccParam.inputRangeDataTopic_ = nh_.param<std::string>(
-			"icp_localization/range_data_topic", "");
+	rangeDataAccParam.numAccumulatedRangeData_ = nh_.param<int>("icp_localization/num_accumulated_range_data", 1);
+	rangeDataAccParam.inputRangeDataTopic_ = nh_.param<std::string>("icp_localization/range_data_topic", "");
 
 	std::cout << "range data parameters: \n";
 	std::cout << "topic: " << rangeDataAccParam.inputRangeDataTopic_ << "\n";
-	std::cout << "num range data accumulated: "
-			<< rangeDataAccParam.numAccumulatedRangeData_ << "\n \n";
+	std::cout << "num range data accumulated: " << rangeDataAccParam.numAccumulatedRangeData_ << "\n \n";
 
 	fixedFrame_ = nh_.param<std::string>("icp_localization/fixed_frame", "map");
 	std::cout << "Setting fixed frame to: " << fixedFrame_ << std::endl;
@@ -215,7 +190,7 @@ void ICPlocalization::initialize() {
 	std::cout << "ICPlocalization: Initialized \n";
 
 	// Set 2D pose subscribe
-	initialPose_ = nh_.subscribe("/initialpose",1, &ICPlocalization::set2DPoseCallback, this);
+	initialPose_ = nh_.subscribe("/initialpose", 1, &ICPlocalization::set2DPoseCallback, this);
 }
 
 DP ICPlocalization::fromPCL(const Pointcloud &pcl) {
@@ -225,8 +200,7 @@ DP ICPlocalization::fromPCL(const Pointcloud &pcl) {
 	return pointmatcher_ros::rosMsgToPointMatcherCloud<float>(ros);
 }
 
-
-const std::string &ICPlocalization::getFixedFrame() const{
+const std::string& ICPlocalization::getFixedFrame() const {
 	return fixedFrame_;
 }
 
@@ -240,11 +214,9 @@ void ICPlocalization::matchScans() {
 	Rigid3d before(lastPosition_, lastOrientation_);
 	if (!isFirstScanMatch_) {
 		const Rigid3d lastPose(initPosition, initOrientation);
-		const Rigid3d motionPoseChange =
-				frameTracker_->getPoseChangeOfRangeSensorInMapFrame(
-						lastOptimizedPoseTimestamp_, regCloudTimestamp_);
-		const Rigid3d motionCorrectedPose = Rigid3d(initPosition,
-				initOrientation) * motionPoseChange;
+		const Rigid3d motionPoseChange = frameTracker_->getPoseChangeOfRangeSensorInMapFrame(
+				lastOptimizedPoseTimestamp_, regCloudTimestamp_);
+		const Rigid3d motionCorrectedPose = Rigid3d(initPosition, initOrientation) * motionPoseChange;
 		initPosition = motionCorrectedPose.translation();
 		initOrientation = motionCorrectedPose.rotation();
 //    std::cout << "Prediction diff: " << motionPoseChange.asString() << std::endl;
@@ -254,24 +226,21 @@ void ICPlocalization::matchScans() {
 // Compute the transformation to express data in ref
 	inputFilters_.apply(regCloud_);
 //	optimizedPose_ = initPose;
-	if(!setPoseFlag){
-		try{
+	if (!isSetPoseFromUser_) {
+		try {
 			initPose = getTransformationMatrix<float>(toFloat(initPosition), toFloat(initOrientation));
 			optimizedPose_ = icp_(regCloud_, initPose);
-		} catch (const std::exception &e){
+		} catch (const std::exception &e) {
 			std::cerr << "Caught exception while scan matching: " << e.what() << std::endl;
 			optimizedPose_ = initPose;
 		}
-	}
-	else	
-	{
-		optimizedPose_ = getTransformationMatrix<float>(toFloat(set_pose_vector_), toFloat(set_pose_quaternion_));
-		setPoseFlag = false;
+	} else {
+		optimizedPose_ = getTransformationMatrix<float>(toFloat(userSetPosition_), toFloat(userSetQuaternion_));
+		isSetPoseFromUser_ = false;
 	}
 
 	optimizedPoseTimestamp_ = regCloudTimestamp_;
-	getPositionAndOrientation<double>(toDouble(optimizedPose_), &lastPosition_,
-			&lastOrientation_);
+	getPositionAndOrientation<double>(toDouble(optimizedPose_), &lastPosition_, &lastOrientation_);
 
 	//limit translation if too big
 //  Rigid3d poseDiffIcp = before.inverse()*Rigid3d(lastPosition_, lastOrientation_);
@@ -285,8 +254,7 @@ void ICPlocalization::matchScans() {
 //  std::cout << "Ground truth diff: " << poseDiffIcp.asString() << "\n\n";
 
 	frameTracker_->setTransformMapToRangeSensor(
-			TimestampedTransform { optimizedPoseTimestamp_, Rigid3d(
-					lastPosition_, lastOrientation_) });
+			TimestampedTransform { optimizedPoseTimestamp_, Rigid3d(lastPosition_, lastOrientation_) });
 	lastOptimizedPoseTimestamp_ = optimizedPoseTimestamp_;
 
 	isFirstScanMatch_ = false;
@@ -295,8 +263,7 @@ void ICPlocalization::matchScans() {
 
 void ICPlocalization::publishPose() const {
 	geometry_msgs::PoseStamped pose_msg;
-	pose_msg.pose = pointmatcher_ros::eigenMatrixToPoseMsg<float>(
-			optimizedPose_);
+	pose_msg.pose = pointmatcher_ros::eigenMatrixToPoseMsg<float>(optimizedPose_);
 	pose_msg.header.frame_id = fixedFrame_;
 	pose_msg.header.stamp = toRos(optimizedPoseTimestamp_);
 	pose_msg.header.seq = seq_++;
@@ -313,9 +280,8 @@ void ICPlocalization::publishPose() const {
 void ICPlocalization::publishRegisteredCloud() const {
 	DP data_out(icp_.getReadingFiltered());
 	icp_.transformations.apply(data_out, optimizedPose_);
-	sensor_msgs::PointCloud2 ros_msg =
-			pointmatcher_ros::pointMatcherCloudToRosMsg<float>(data_out,
-					fixedFrame_, ros::Time::now());
+	sensor_msgs::PointCloud2 ros_msg = pointmatcher_ros::pointMatcherCloudToRosMsg<float>(data_out, fixedFrame_,
+			ros::Time::now());
 	registeredCloudPublisher_.publish(ros_msg);
 }
 
@@ -326,15 +292,13 @@ void ICPlocalization::icpWorker() {
 			r.sleep();
 			continue;
 		}
-		regCloudTimestamp_ =
-				rangeDataAccumulator_.getAccumulatedRangeDataTimestamp();
+		regCloudTimestamp_ = rangeDataAccumulator_.getAccumulatedRangeDataTimestamp();
 		regCloud_ = rangeDataAccumulator_.popAccumulatedRangeData().data_;
 		namespace ch = std::chrono;
 		const auto startTime = ch::steady_clock::now();
 		matchScans();
 		const auto endTime = ch::steady_clock::now();
-		const unsigned int nUs = ch::duration_cast<ch::microseconds>(
-				endTime - startTime).count();
+		const unsigned int nUs = ch::duration_cast<ch::microseconds>(endTime - startTime).count();
 		const double timeMs = nUs / 1000.0;
 //    std::string infoStr = "Scan matching took: " + std::to_string(timeMs) + " ms \n";
 //    ROS_INFO_STREAM(infoStr);
